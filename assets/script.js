fd7ab1a6dabecf72119f88629c262545a26ac9f5
xpertBoy <70654510+xpertBoy@users.noreply.github.com>
2020-09-15 17:11:28 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+var f_name = document.getElementById("f-name");
+var l_name = document.getElementById("l-name");
+var e_mail = document.getElementById("e-mail");
+var pa_ss = document.getElementById("pa-ss");
 
+
+function setDefaults()
+{
+    f_name.value = "";
+    l_name.value = "";
+    e_mail.value = "";
+    pa_ss.value = "";
+}
