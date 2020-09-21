94a62b2e2616e5298008f885d0216b83004beae6
xpertBoy <70654510+xpertBoy@users.noreply.github.com>
2020-09-21 16:30:18 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var f_name = document.getElementById("f-name");
 var l_name = document.getElementById("l-name");
 var e_mail = document.getElementById("e-mail");
 var pa_ss = document.getElementById("pa-ss");
-
+const text = document.getElementById("savedText");
 
 function setDefaults()
 {
@@ -23,4 +23,25 @@ function cookieAccepted()
 {
     cookieBanner.style.bottom = "-100%";
     localStorage.setItem('cookieAccepted', 'true');
+}
+
+let data = "";
+
+function save()
+{
+    data = data + "First name: " + f_name.value +
+                  ", Last name: " + l_name.value +
+                  ", Email: " + e_mail.value +
+                  ", Password: " + pa_ss.value;
+    f_name.value = "";
+    l_name.value = "";
+    e_mail.value = "";
+    pa_ss.value = "";
+
+    localStorage.setItem('data', data);
+}
+
+function load()
+{
+    text.innerHTML = localStorage.getItem('data');
 }
